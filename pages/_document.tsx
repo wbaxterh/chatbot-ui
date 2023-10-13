f886a585d6ac3d7f44ff92f86199bf150e6a63ed
Wesley Huber <wesleybaxterhuber@gmail.com>
2023-10-13 00:18:01 -0400

    adding seo tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,50 @@ export default function Document(props: Props) {
     <Html lang={currentLocale}>
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Chatbot No Login"
+        ></meta>
+        {/* Basic SEO Meta Tags */}
+        <title>
+          ChatGPT No Login - Interact with ChatGPT without Logging In
+        </title>
+        <meta
+          name="description"
+          content="Interact with ChatGPT without the need to log in. Use our API keys and get started instantly."
+        />
+        <meta name="keywords" content="ChatGPT, No Login, API keys, Chatbot" />
+
+        {/* Open Graph Meta Tags for Social Media */}
+        <meta
+          property="og:title"
+          content="ChatGPT No Login - Interact with ChatGPT without Logging In"
+        />
+        <meta
+          property="og:description"
+          content="Interact with ChatGPT without the need to log in. Use our API keys and get started instantly."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.chatgptnologin.com" />
+        <meta
+          property="og:image"
+          content="https://www.chatgptnologin.com/og-image.jpg"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="ChatGPT No Login - Interact with ChatGPT without Logging In"
+        />
+        <meta
+          name="twitter:description"
+          content="Interact with ChatGPT without the need to log in. Use our API keys and get started instantly."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.chatgptnologin.com/twitter-image.jpg"
+        />
       </Head>
       <body>
         <Main />
