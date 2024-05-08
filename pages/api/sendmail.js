51dbab409f3e5b76f3d1e414b1528dcd8965a258
wbaxterh <wesleybaxterhuber@gmail.com>
2024-05-08 12:52:39 -0400

    adding more error handling

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -29,10 +29,11 @@ export default async (req, res) => {
       res.status(200).json({ message: 'Email sent successfully' });
     } catch (error) {
       console.error('Failed to send email', error);
-      res.status(500).json({ message: 'Failed to send email' });
+      res
+        .status(500)
+        .json({ message: 'Failed to send email', error: error.message });
     }
   } else {
-    // Handle any other HTTP method
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
